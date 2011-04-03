cb1a1d58ff8d28d38bf3998d17586ee7c873ec81
Daniel Tull <dt@danieltull.co.uk>
2011-04-03 19:15:57 +0100

    Add app target

--- DCTGravatarApp/main.m
+++ DCTGravatarApp/main.m
@@ -1,8 +1,8 @@
 //
 //  main.m
-//  DCTGravatar
+//  DCTGravatarApp
 //
-//  Created by Daniel Tull on 7.03.2011.
+//  Created by Daniel Tull on 3.04.2011.
 //  Copyright 2011 Daniel Tull. All rights reserved.
 //
 
