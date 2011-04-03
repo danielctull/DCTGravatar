2db711263d168fe0d4420177894a4036f962e65d
Daniel Tull <dt@danieltull.co.uk>
2011-04-03 19:05:47 +0100

    Add new project

---
+++ DCTGravatarTests/DCTGravatarTests.h
@@ -0,0 +1,17 @@
+//
+//  DCTGravatarTests.h
+//  DCTGravatarTests
+//
+//  Created by Daniel Tull on 3.04.2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface DCTGravatarTests : SenTestCase {
+@private
+    
+}
+
+@end
